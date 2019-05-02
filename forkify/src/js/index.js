a0d939da050181237646b828b9c21924f6f42524
Aleksandar Stanchev <aleksandar.stanchev@intercard.bg>
2019-05-02 17:45:55 +0300

    forkify

---
+++ forkify/src/js/index.js
@@ -0,0 +1,39 @@
+// Global app controller
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
+
+// Global state of the app
+//   - Search object
+//   - Current recipe object
+//   - Shopping list object
+//   - Liked recipes
+// console.log(searchView);
+const state = {};
+
+const controlSearch = async () => {
+    // 1. Get query form view
+    const query = searchView.getInput();
+
+    if (query) {
+        // 2. New search object and add the state
+        state.search = new Search(query);
+
+        // 3. Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        // 4. Search for recipes
+        await state.search.getResult();
+
+        // 5. Render results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+};
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
